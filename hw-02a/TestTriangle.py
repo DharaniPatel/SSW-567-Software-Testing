092fe5f93908b5516bf8a0b29157a19ce6b6de62
DharaniPatel <147118756+DharaniPatel@users.noreply.github.com>
2024-10-04 19:06:53 -0400

    Updated Triangle & TestTriangle

--- hw-02a/TestTriangle.py
+++ hw-02a/TestTriangle.py
@@ -1,21 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple unittest implementation
-
-@author: jrr
-@author: rk
-"""
-
 import unittest
 
 from Triangle import classifyTriangle
 
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
-
 class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
         self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
@@ -26,7 +13,24 @@ class TestTriangles(unittest.TestCase):
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
 
+    def testIsocelesTriangles(self): 
+        self.assertEqual(classifyTriangle(4,4,5),'Isoceles','4,4,5 is an Isoceles triangle')
+
+    def testScaleneTriangles(self): 
+        self.assertEqual(classifyTriangle(3,4,6),'Scalene','3,4,6 is a Scalene triangle')
+
+    def testNotATriangle(self): 
+        self.assertEqual(classifyTriangle(1,2,4),'NotATriangle','1,2,4 is not a triangle')
+
+    def testInvalidInput(self): 
+        self.assertEqual(classifyTriangle(-1,2,3),'InvalidInput','-1 is invalid')
+
+    def testNonIntegerInput(self): 
+        self.assertEqual(classifyTriangle(1.5,2,3),'InvalidInput','1.5 is invalid')
+
+    def testLargeInput(self): 
+        self.assertEqual(classifyTriangle(201,2,3),'InvalidInput','201 is invalid')
+
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
